07641b5966ed2f529f0eeab98e3c80241926bbe7
zihad002 <zihad.mdbfitlab@gmail.com>
2023-07-21 18:51:30 +0600

    updated files

---
+++ js/izi_toast_alert.js
@@ -0,0 +1,6 @@
+function iziToster01(){
+    iziToast.info({
+        title: 'Info about yourself',
+        message: 'What would you like to know about the islam?',
+    });
+}
---
+++ js/sweetalert.js
@@ -0,0 +1,19 @@
+function sweetAlert01(){
+    // Row JavaScript 
+    // alert ("Alhamduillah it' working");
+    swal({
+        title: "Are you pray fazor salath?",
+        Text: "Are you wake up early?",
+        icon: "warning"
+    });
+}
+
+
+// Sweet Alert 2 -->
+function sweetAlertTwo01(){
+    Swal.fire({
+        title: "Are you ready for Kobor?",
+        text: "Nop! I am very worry about it...",
+        icon: "info"
+    });
+}
